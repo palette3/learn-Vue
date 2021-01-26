111f814bbf3ebe7cce83138f8aad953f0043fc4b
sjy <1422724727@qq.com>
2021-01-26 16:08:21 +0800

    点击计数添加属性

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,68 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div>
+		<h2>点击计数案例</h2>
+		<p>
+			<button @click="reduce">-</button>
+			<span>{{num}}</span>
+			<button @click="add">+</button>
+		</p>
+		<p>
+			<button @click="num2down">-</button>
+			<span>{{num2}}</span>
+			<button @click="num2up">+</button>
+		</p>
+	</div>
+	<div>
+		<br>
+		<h1>使用方法：<span>{{normal()}}</span></h1>
+	</div>
+	<div>
+		<br>
+		<h1>使用方法：<span>{{comput1}}</span></h1>
+	</div>
+	
   </div>
 </template>
+
+
+<script>
+	export default{
+		data(){
+			return{
+				num:0,
+				num2:0
+			}
+		},
+		methods:{
+			normal(){
+				console.log("普通方法执行了");
+				return"普通方法"
+			},
+			reduce(){
+				this.num--;
+			},
+			add(){
+				this.num++;
+			},
+			num2down(){
+				this.num2--;
+			},
+			num2up(){
+				this.num2++;
+			}
+		},
+		computed:{
+			comput1(){
+				console.log("计算属性执行了");
+				return"计算属性"+this.num
+			}
+		},
+		watch:{
+			num(newValue,oldValue){
+				console.log("监听到num变化了",newValue,oldValue)
+			}
+		}
+		
+	}
+</script>
